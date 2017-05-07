9ccd096989c6f9c44d86c05fb9e3a6978397a1a6
YLCFQ <chen41384406@gmail.com>
2017-05-06 19:21:03 -0700

    Add multipart video uploading

--- node/routes/upload.js
+++ node/routes/upload.js
@@ -1,13 +1,29 @@
 var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart({ uploadDir: '/uploading'});
+var multipartMiddleware = multipart();
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.post('/', multipartMiddleware, function(req, res){
 	console.log("Hello");
-	var files = req.files;
-	console.log(files.upload.path);
 
+	var files = req.files;
+	var body = req.body;
+	var milliseconds = (new Date).getTime();
+	console.log(milliseconds);
+  		fs.createReadStream(files.files.path).pipe(fs.createWriteStream('/Users/chenyulong/Documents/dvp/node/routes/movie/' + milliseconds +'.mp4'));
+		fs.unlink(files.files.path, function(err){
+			if (err) throw err;
+		});
+		
+
+	return res.json({
+		success: true,
+		message: "Registration successful!"
+
+	
+}); 
+	
 });
 
 
